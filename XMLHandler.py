fb663759c80f2427c325f4c37ed8a3e5c73a7710
barifrah1 <63462394+barifrah1@users.noreply.github.com>
2022-01-04 16:07:05 +0200

    get images coordinates and page number

--- XMLHandler.py
+++ XMLHandler.py
@@ -57,29 +57,52 @@ class XMLHandler:
 
     def find_all_images_in_xml(self):
         self.images_elements = []
+        self.image_data = {}
+        self.page_numbers = [0]
         for elem in self.tree.iter():
             if(elem.tag == self.ns["w"]+'drawing'):
                 rId = -1
-                cords = {'x': -1, 'y': -1}  # cordinates
+                cords = {'page': -1, 'x': -1, 'y': -1}  # cordinates
                 drawing = elem
                 for e in drawing.iter():
                     if(e.tag == self.ns["a"]+'blip'):
                         ns = Utils.add_curly_braces_to_string(e.nsmap["r"])
                         rId = (e.attrib[ns+'embed'])
+                        p_element = self.get_ancestor_by_type(
+                            e, self.ns["w"]+'p')
+                        try:
+                            ppr_element = p_element.find(self.ns['w']+'pPr')
+                            wframe_element = ppr_element.find(
+                                self.ns['w']+'framePr')
+                            cords['page'] = self.page_numbers[-1]
+                            cords['x'] = wframe_element.attrib[self.ns['w']+'x']
+                            cords['y'] = wframe_element.attrib[self.ns['w']+'y']
+                            self.image_data[rId] = cords
+                        except Exception as e:
+                            print(e)
+                            raise(e)
                 self.images_elements.append(elem)
-        # print(self.images_elements)
+            elif(elem.tag == self.ns["w"]+'footnotePr'):
+                self.page_numbers.append(self.page_numbers[-1]+1)
+        print(self.image_data)
 
     def build_namespaces_dict(self):
         self.ns["w"] = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}"
         self.ns["a"] = "{http://schemas.openxmlformats.org/drawingml/2006/main}"
-        self.ns["a"] = "{http://schemas.openxmlformats.org/drawingml/2006/main}"
+
+    # for example tag = <a:blip> and type_tage=<Drawing> wil return the ancestor of <a:blip> that is from type <Drawing>
+    def get_ancestor_by_type(self, tag, type_tag):
+        parent = tag.getparent()
+        while(parent.tag != type_tag or parent.tag == self.ns["w"]+'document'):
+            parent = parent.getparent()
+        return parent
 
     if __name__ == '__main__':
         try:
             from XMLHandler import XMLHandler
             xml = XMLHandler('40-09-19')
             xml.find_all_images_in_xml()
-            # rmtree('./'+XML_FOLDER+'/'+'40-09-19')
+            rmtree('./'+XML_FOLDER+'/'+'40-09-19')
         except Exception as e:
             rmtree('./'+XML_FOLDER+'/'+'40-09-19')
             raise(e)
